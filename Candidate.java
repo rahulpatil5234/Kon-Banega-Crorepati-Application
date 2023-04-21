bb0a6baaa2019b71d8b2dc8b6f57b870738932c1
Rahul Patil <120298377+rahulpatil5234@users.noreply.github.com>
2023-04-21 15:59:50 +0530

    Add files via upload

---
+++ Candidate.java
@@ -0,0 +1,72 @@
+package com.bldea.crorepathiapp.package1;
+
+public class Candidate {
+	private String name;
+	private int age;
+	private String city;
+	private String state;
+	private static int amount;
+	/**
+	 * @param name
+	 * @param age
+	 * @param city
+	 * @param state
+	 */
+	public Candidate(String name, int age, String city, String state) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.city = city;
+		this.state = state;
+		
+	}
+	
+	
+	/**
+	 * @param amount the amount to set
+	 */
+	public static void setAmount(int amt) {
+		amount = amt;
+	}
+	/**
+	 * @return the amount
+	 */
+	public int getAmount() {
+		return amount;
+	}
+
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+
+
+	/**
+	 * @return the state
+	 */
+	public String getState() {
+		return state;
+	}
+
+	
+
+}
